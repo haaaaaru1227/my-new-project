854bd8b28547f801afb88d5266ebf0532d556e8f
haaaaaru1227 <harucolor84@icloud.com>
2024-12-05 22:15:02 +0900

    Update webpack environment configuration

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
+const { resolve } = require('path')
+
+const customConfig = {
+  resolve: {
+    alias: {
+      '@': resolve('app/javascript')
+    }
+  }
+}
+
+environment.config.merge(customConfig)
 
 module.exports = environment
