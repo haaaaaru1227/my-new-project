83cc069c0775d28d7b647bfd9df09673e69b493c
haaaaaru1227 <harucolor84@icloud.com>
2024-12-05 14:46:30 +0900

    Precompiled assets for production

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,3 @@
-const { environment } = require('@rails/webpacker');
+const { environment } = require('@rails/webpacker')
 
-// エントリポイントを明示的に追加
-environment.config.merge({
-  entry: {
-    application: './app/javascript/packs/application.js', // 必要なエントリポイントを追加
-  },
-});
-
-module.exports = environment;
+module.exports = environment
